22638f2100dfdb1a05f41c747f9f70816212bfeb
Roman Gruba <roman.gruba@gmail.com>
2020-01-27 23:15:13 +0200

    adding images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
+import Colors from "../constants/colors";
 
 const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
-      <Text>The game is over!</Text>
-      <Text>Number of rounds: {props.roundsNumber}</Text>
-      <Text>Number was: {props.userNumber}</Text>
+      <TitleText>The game is over!</TitleText>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          // source={require("../assets/images/success.png")}
+          source={{
+            uri:
+              "https://upload.wikimedia.org/wikipedia/commons/a/ac/Crystal_Project_success.png"
+          }}
+          resizeMode="cover"
+        />
+      </View>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone needed{" "}
+          <Text style={styles.highlight}> {props.roundsNumber} </Text> rounds to
+          guess number <Text style={styles.highlight}>{props.userNumber}</Text>!
+        </BodyText>
+      </View>
       <Button title="NEW GAME" onPress={props.onNewGame} />
     </View>
   );
@@ -17,6 +36,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  imageContainer: {
+    width: "40%",
+    height: 100,
+    marginVertical: 20,
+    overflow: "hidden"
+  },
+  image: {
+    width: "100%",
+    height: "100%"
+  },
+  highlight: {
+    fontWeight: "bold",
+    color: Colors.primary
+  },
+  resultContainer: {
+    marginVertical: 10,
+    marginHorizontal: 30
+  },
+  resultText: {
+    textAlign: "center"
   }
 });
 
