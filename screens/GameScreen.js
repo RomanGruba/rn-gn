3fed0744b627002edb45764bc738351e3ade9566
Roman Gruba <roman.gruba@gmail.com>
2020-01-28 23:53:55 +0200

    scroll history

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -11,6 +11,7 @@ import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
 import { Ionicons } from "@expo/vector-icons";
 import MainButton from "../components/MainButton";
+import BodyText from "../components/BodyText";
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -22,6 +23,13 @@ const generateRandomBetween = (min, max, exclude) => {
     return rndNum;
   }
 };
+
+const renderListItem = (value, numOfRound) => (
+  <View key={value} style={styles.listItem}>
+    <BodyText>#{numOfRound}</BodyText>
+    <BodyText>{value}</BodyText>
+  </View>
+);
 
 const GameScreen = props => {
   const initialGuess = generateRandomBetween(1, 100, props.userChoice);
@@ -77,13 +85,13 @@ const GameScreen = props => {
           <Ionicons name="md-add" size={24} color="" white />
         </MainButton>
       </Card>
-      <ScrollView>
-        {pastGuesses.map(guess => (
-          <View key={guess}>
-            <Text>{guess}</Text>
-          </View>
-        ))}
-      </ScrollView>
+      <View style={styles.list}>
+        <ScrollView>
+          {pastGuesses.map((guess, index) =>
+            renderListItem(guess, pastGuesses.length - index)
+          )}
+        </ScrollView>
+      </View>
     </View>
   );
 };
@@ -100,6 +108,20 @@ const styles = StyleSheet.create({
     marginTop: 20,
     width: 300,
     maxWidth: "80%"
+  },
+  list: {
+    width: "80%",
+    flex: 1
+  },
+
+  listItem: {
+    borderRightColor: "#ccc",
+    padding: 15,
+    marginVertical: 10,
+    backgroundColor: "white",
+    borderWidth: 1,
+    flexDirection: "row",
+    justifyContent: "space-around"
   }
 });
 
